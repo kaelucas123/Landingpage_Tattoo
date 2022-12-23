2ae85be4187a975daaefe7aa51356c74c4c8dc69
Kae Lucas <kaeteodoro@gmail.com>
2022-12-23 15:43:41 -0300

    Adicionando components

---
+++ src/Pages/LandingPage/LandingPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from '../LandingPage/LandingPage.module.css'
+
+function LandingPage() {
+  return (
+    <div id={styles.container}>
+        <section id={styles.bgImage}></section>
+        
+    </div>
+  )
+}
+
+export default LandingPage
