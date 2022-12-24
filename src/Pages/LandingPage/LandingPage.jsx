30d77bbf6a5fb9d092a24b4ff66e39da55e9aacc
Kae Lucas <kaeteodoro@gmail.com>
2022-12-24 13:18:39 -0300

    Feito component hero

--- src/Pages/LandingPage/LandingPage.jsx
+++ src/Pages/LandingPage/LandingPage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import styles from '../LandingPage/LandingPage.module.css'
 
+import Hero from '../../Components/Hero/Hero'
+
 function LandingPage() {
   return (
     <div id={styles.container}>
         <section id={styles.bgImage}></section>
-        
+        <Hero></Hero>
     </div>
   )
 }
