880d5d921b49d862d6ec5f41791ee6be8485a30e
Kae Lucas <kaeteodoro@gmail.com>
2022-12-24 22:00:26 -0300

    Menu responsivo

--- src/Pages/LandingPage/LandingPage.jsx
+++ src/Pages/LandingPage/LandingPage.jsx
@@ -4,19 +4,21 @@ import styles from '../LandingPage/LandingPage.module.css'
 // import M
 import Hero from '../../Components/Hero/Hero'
 import Sobre_Mim from '../../Components/Sobre_Mim/Sobre_Mim'
+import MenuBurguer from '../../Components/MenuBurguer/MenuBurguer'
 
 function LandingPage() {
   return (
     <div id={styles.container}>
-      
-        <section id={styles.bgImage}>
 
-          <Hero></Hero>
-          <div className={styles.divisor}></div>
-          <Sobre_Mim></Sobre_Mim>
-          <div className={styles.divisor}></div>
+      <section id={styles.bgImage}>
+        <MenuBurguer></MenuBurguer>
+        
+        <Hero></Hero>
+        <div className={styles.divisor}></div>
+        <Sobre_Mim></Sobre_Mim>
+        <div className={styles.divisor}></div>
 
-        </section>
+      </section>
     </div>
   )
 }
